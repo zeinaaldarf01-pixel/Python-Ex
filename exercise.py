cc3dac48065d5a19c83f3f96db7c6fdf1af5d520
zeinaaldarf01-pixel <zeinaaldarf01@gmail.com>
2025-11-25 20:48:05 +0100

    initial commit

--- exercise.py
+++ exercise.py
@@ -1,2 +1,61 @@
 print("hello")
 print("hello2")
+name=zeina
+age=24
+is_student=true
+print (name)
+print (age)
+print (is_student)
+task 2
+city=rome
+temperature=18
+is_sunny=true
+print("the weather in", city,temperature,is_sunny if temperature=>18)
+
+task3 
+num1=8
+num2=2
+print("sum:", num+num2)
+print("difference:", num1-num2)
+print("product:", num1*num2)
+print("division:", num1/num2)
+
+task4
+first_name: "Zeina"
+last_name: "Aldarf"
+full_name= first_name+last_name
+print("full name:", full_name)
+
+task 5
+price=3.50
+quantity=2
+total cost=price*quantity
+print("total cost:",total cost)
+
+task 6
+length=5
+width=3
+area=length*width   
+print("area of rectangle:", area)
+perimeter=2*(length+width)
+print("perimeter of rectangle:", perimeter)
+task 7
+year_of_birth=2000
+current_year=2025
+age=current_year-year_of_birth
+print("age:", age)
+task 8
+product_name = "Notebook"
+product_price = 2.50
+in_stock = True
+
+print("Product:", product_name, "Price:", product_price, "In stock:", in_stock)
+task 9
+KILOMETERES=10
+MILES=KILOMETERES*0.621371
+print("miles:", MILES)
+task 10
+HOURS WORKED=40
+HOURLY RATE=10
+TOTAL PAY=HOURS WORKED*HOURLY RATE
+print("total pay:", TOTAL PAY)
